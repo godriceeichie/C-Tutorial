a8169568ead7c66b414f257a75d57fe87b8aa480
godriceeichie <emmaeichie@gmail.com>
2023-11-13 13:02:12 +0100

    Done with the task

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿internal class HashSetClass
+{
+    static Dictionary<string, int> products = new Dictionary<string, int>();
+    static Dictionary<string, int> MarketProgram(string product, int price)
+    {
+        if (products.ContainsKey(product))
+        {
+            products[product]++;
+        }
+        else
+        {
+            products.Add(product, price);
+        }
+        return products;
+    }
+    static void PrintProducts()
+    {
+        foreach (KeyValuePair<string, int> kvp in products)
+        {
+            Console.WriteLine("Product: " + kvp.Key);
+            Console.WriteLine("Price: " + kvp.Value);
+        }
+    }
+    private static void Main()
+    {
+        bool tryAgain = true;
+        do
+        {
+            Console.WriteLine("Enter your product");
+            string product = Console.ReadLine();
+            Console.WriteLine("Enter product quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            HashSetClass.MarketProgram(product.Trim(), quantity);
+
+            Console.WriteLine("Are you done (Yes or No)");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() == "yes" || answer.ToLower() == "y")
+            {
+                HashSetClass.PrintProducts();
+                tryAgain = false;
+
+            }
+            else if (answer.ToLower() == "no" || answer.ToLower() == "n")
+            {
+                tryAgain = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Answer");
+            }
+        } while (tryAgain);
+
+     
+
+    }
+}
